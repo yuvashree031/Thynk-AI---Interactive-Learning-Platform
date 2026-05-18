1ac58ff1a7da7c2cea6d4d6264eea3d0e382ccd8
Yuvashree R <yuvashree.ra05@gmail.com>
2026-05-18 08:37:26 +0530

    Comment out unused import in SecurityConfig.java
    Commented out an import statement in SecurityConfig.java

--- src/main/java/com/ailearning/config/SecurityConfig.java
+++ src/main/java/com/ailearning/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ailearning.config;
 
+//import
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
