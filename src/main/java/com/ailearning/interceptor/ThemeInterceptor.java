3d24f94b656e4b7faf7e79d70555a3ed272aa31d
Yuvashree R <yuvashree.ra05@gmail.com>
2026-05-25 13:58:04 +0530

    Update ThemeInterceptor.java

--- src/main/java/com/ailearning/interceptor/ThemeInterceptor.java
+++ src/main/java/com/ailearning/interceptor/ThemeInterceptor.java
@@ -1,6 +1,5 @@
 package com.ailearning.interceptor;
 
-//import
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
