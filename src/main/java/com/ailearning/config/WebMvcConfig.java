5ff709d4ce7089f6ae75396f20eb16de198a897a
Yuvashree R <yuvashree.ra05@gmail.com>
2026-06-03 08:52:46 +0530

    Remove unused import statement in WebMvcConfig

--- src/main/java/com/ailearning/config/WebMvcConfig.java
+++ src/main/java/com/ailearning/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.ailearning.config;
 
-//import
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
