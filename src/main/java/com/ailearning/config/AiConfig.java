9bbfebc7336d5cf2f2dce0791267b1322339305a
Yuvashree R <yuvashree.ra05@gmail.com>
2026-06-22 19:48:01 +0530

    Remove unnecessary comment from AiConfig.java
    Removed commented line from AiConfig.java

--- src/main/java/com/ailearning/config/AiConfig.java
+++ src/main/java/com/ailearning/config/AiConfig.java
@@ -1,6 +1,5 @@
 package com.ailearning.config;
 
-//123
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
