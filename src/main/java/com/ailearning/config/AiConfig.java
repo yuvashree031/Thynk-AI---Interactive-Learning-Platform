9c9352119bf96e74cc6d5369d7433e7f80448bd8
Yuvashree R <yuvashree.ra05@gmail.com>
2026-06-22 19:47:00 +0530

    Refactor AiConfig.java for formatting and comments
    Removed unnecessary blank line and added a comment.

--- src/main/java/com/ailearning/config/AiConfig.java
+++ src/main/java/com/ailearning/config/AiConfig.java
@@ -1,6 +1,6 @@
 package com.ailearning.config;
 
-
+//123
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
