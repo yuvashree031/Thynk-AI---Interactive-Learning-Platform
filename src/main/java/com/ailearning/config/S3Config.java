300c3bacb96efacd8186b6271d94b4d5667e10b1
Yuvashree R <yuvashree.ra05@gmail.com>
2026-06-22 19:47:25 +0530

    Update S3Config.java

--- src/main/java/com/ailearning/config/S3Config.java
+++ src/main/java/com/ailearning/config/S3Config.java
@@ -1,6 +1,6 @@
 package com.ailearning.config;
 
-//123
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
