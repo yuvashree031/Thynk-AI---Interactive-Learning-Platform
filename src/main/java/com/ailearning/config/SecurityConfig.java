cde00f12d5248cbc3d7aa605482a6b90b0469cd1
Yuvashree R <yuvashree.ra05@gmail.com>
2026-07-15 13:06:23 +0530

    Update SecurityConfig.java

--- src/main/java/com/ailearning/config/SecurityConfig.java
+++ src/main/java/com/ailearning/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.ailearning.config;
-//imp
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
