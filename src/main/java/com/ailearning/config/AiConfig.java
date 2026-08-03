403166294ef59289626c0e582f408f80bed0daba
Yuvashree R <yuvashree.ra05@gmail.com>
2026-08-03 12:33:47 +0530

    Update AiConfig.java

--- src/main/java/com/ailearning/config/AiConfig.java
+++ src/main/java/com/ailearning/config/AiConfig.java
@@ -1,5 +1,5 @@
 package com.ailearning.config;
-//impsddsfhfjfad
+//impsddsfhf
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
